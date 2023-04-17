045b9e23d129625243738017383f1fa0d4905d79
huyhunhngc <huyhn@hybrid-technologies.vn>
2023-04-17 10:58:53 +0700

    test remote

--- cmd/main.go
+++ cmd/main.go
@@ -10,19 +10,12 @@ import (
 )
 
 func main() {
-
 	app := bootstrap.App()
-
 	env := app.Env
-
 	db := app.Mongo.Database(env.DBName)
 	defer app.CloseDBConnection()
-
 	timeout := time.Duration(env.ContextTimeout) * time.Second
-
 	gin := gin.Default()
-
 	route.Setup(env, timeout, db, gin)
-
 	gin.Run(env.ServerAddress)
 }
